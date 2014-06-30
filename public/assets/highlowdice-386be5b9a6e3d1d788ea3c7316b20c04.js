7768c58e076aaf0cb25a31f4fb231205340e7df0
kpmoy88 <kpmoy88@gmail.com>
2014-06-30 16:08:06 -0400

    Rake assets

--- public/assets/highlowdice-386be5b9a6e3d1d788ea3c7316b20c04.js
+++ public/assets/highlowdice-386be5b9a6e3d1d788ea3c7316b20c04.js
@@ -3,6 +3,7 @@
  * 
  * Made by Kenneth Moy, kpmoy88@gmail.com, Bronx, New York, 06.2014
  */
+
  
 $('.playgame').click(startgame);
 
@@ -572,3 +573,4 @@ $.fn.center = function () {
     this.css("left", ( $(window).width() - this.width() ) / 2+$(window).scrollLeft() + "px");
     return this;
 }
+;
